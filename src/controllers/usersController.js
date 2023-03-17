57e25fdb95d5c1ef2258f9e6bc2eb4e2d0ac8c70
franSlepoy <slepoyfrancisco@gmail.com>
2023-03-17 11:02:53 -0300

    validaciones de registro completas

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,10 +1,23 @@
 const path = require("path");
+const { validationResult } = require("express-validator")
 
 const usersController = {
         
         registro: (req, res) => {
             res.render(path.join(__dirname, "../views/users/registro"));
         },
+        procesoRegistro: (req, res) =>{
+            const resultValidation = validationResult(req);
+            
+            if(resultValidation.errors.length > 0){
+                return res.render(path.join(__dirname, "../views/users/registro"),
+                {errors: resultValidation.mapped(),
+                oldData: req.body   
+                });
+            }
+            res.send("Bien ahí, complestaste bien la información");
+        },
+
         login:  (req, res) => {
             res.render(path.join(__dirname, "../views/users/login"));
         },
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const { body } = require("express-validator");
 
 const usersController = require("../controllers/usersController");
 
+const validations = [
+    body("nombreCompleto").notEmpty().withMessage("Tenés que escribir tu nombre comleto"),
+    body("email").notEmpty().withMessage("Tenés que escribir tu email").bail()
+                 .isEmail().withMessage("Tenés que escribir un formato válido"),
+    body("contraseña").notEmpty().withMessage("Tenés que escribir una contraseña"),
+    //body("pais").notEmpty.withMessage("Tenés que elegir un país"),
+];
+
 router.get("/registro", usersController.registro);
+router.post("/registro", validations, usersController.procesoRegistro);
+
 router.get("/login", usersController.login);
 router.get("/profile/:userId", usersController.registro);
 
